431cd19385c1d35316565ce3d484d5fffd09f887
Mark Sowden <hogsy@oldtimes-software.com>
2021-05-12 22:06:52 +0100

    Added placeholder templatechecksum.c file, just to let us compile this crap.

---
+++ OniProj/OniWin32Projs/ImpConsole/templatechecksum.c
@@ -0,0 +1,10 @@
+#include "BFW.h"
+#include "BFW_TemplateManager.h"
+#include "BFW_TM_Private.h"
+
+UUtUns64 TMgTemplateChecksum = 1234;
+UUtUns32 TMgNumTemplateDefinitions = 0;
+TMtTemplateDefinition* TMgTemplateDefinitionArray = NULL;
+
+void TMrTemplate_BuildList(void) {
+}
